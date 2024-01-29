fc1d0b693480d2650638f5dc23d41df8387c59ca
Alex <71541578+eXtract44@users.noreply.github.com>
2024-01-29 15:16:33 +0100

    Update buttons_func.ino

--- buttons_func.ino
+++ buttons_func.ino
@@ -5,6 +5,5 @@ void ini_buttons(){
 pinMode(TOUCH_PIN, INPUT);
 }
 bool read_button_1(){
-     if (digitalRead(TOUCH_PIN) == true) return true;
-     else return false;  
+     return digitalRead(TOUCH_PIN);  
 }
